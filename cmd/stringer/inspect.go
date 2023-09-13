6e539c43e61b5be21a155c3b3fbc683b71ff91f8
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-12 22:15:40 -0300

    feat: Create new cobra.Command called 'inspect' to return string length and the string digits

---
+++ cmd/stringer/inspect.go
@@ -0,0 +1,26 @@
+package stringer
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/vitor-augusto1/stringer-cli/pkg/stringer"
+)
+
+var onlyDigits bool
+var inspectCmd = &cobra.Command{
+  Use: "inspect",
+  Aliases: []string{"insp"},
+  Short: "Inspects a string",
+  Args: cobra.ExactArgs(1),
+  Run: func(cmd *cobra.Command, args []string) {
+    i := args[0]
+    res, kind := stringer.Inspect(i, onlyDigits)
+    pluralS := "s"
+    if res == 1 {
+      pluralS = ""
+    }
+    fmt.Printf("'%s' has a %d %s%s.\n", i, res, kind, pluralS)
+  },
+}
+
