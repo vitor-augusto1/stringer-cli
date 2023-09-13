63297abd2482d6111e5ed2a1a5afc8969fe6860e
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-12 22:17:28 -0300

    feat: Create new cobra.Command called 'reverse' to reverse strings

---
+++ cmd/stringer/reverse.go
@@ -0,0 +1,24 @@
+package stringer
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/vitor-augusto1/stringer-cli/pkg/stringer"
+)
+
+
+
+
+var reverseCmd = &cobra.Command{
+  Use: "reverse",
+  Aliases: []string{"rev"},
+  Short: "Reverses a string",
+  Args: cobra.ExactArgs(1),
+  Run: func(cmd *cobra.Command, args []string) {
+    res := stringer.Reverse(args[0])
+    fmt.Println(res)
+  },
+}
+
+
